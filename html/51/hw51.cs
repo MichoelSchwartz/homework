3eccd11f416673159cdeb927f15813c95b9a2b57
MichoelSchwartz <michoeldov@gmail.com>
2024-05-27 21:24:08 -0400

    hw51

---
+++ html/51/hw51.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace SQL
+{
+	public class Shirt
+	{
+		public string Color { get; set; }
+		public string Pattern { get; set; }
+	}
+	public class UsingShirt
+	{
+		string[] colors = new string[] { "red", "green", "blue" };
+		string[] patterns = new string[] { "striped", "checked", "plain" };
+
+		public UsingShirt()
+		{
+            for (int i = 0; i < colors.Length; i++)
+            {
+				for (int j = 0; j < patterns.Length; j++)
+				{
+					Shirt s = new Shirt();
+					s.Color = colors[i];
+					s.Pattern = patterns[j];
+					Console.WriteLine($"{s.Color}:{s.Pattern}");
+				}
+            }
+        }
+
+		public static void Main(string[] args)
+		{
+			new UsingShirt();
+		}
+	}
+}
+
