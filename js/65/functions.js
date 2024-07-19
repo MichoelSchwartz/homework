1617d86728816c622501127b68fdcf723baa2bcf
MichoelSchwartz <michoeldov@gmail.com>
2024-07-18 23:06:01 -0400

    hw65

---
+++ js/65/functions.js
@@ -0,0 +1,17 @@
+'use strict';
+
+function CtoF(x) {
+    return (x / 5) * 9 + 32;
+}
+function FtoC(x) {
+    return ((x - 32) * 5) / 9;
+}
+
+console.log((CtoF(67)));
+console.log((FtoC(67)));
+console.log(CtoF(100));
+console.log(FtoC(100));
+
+let x = prompt('enter a temperature to convert from F to C')
+alert(`the celcius value for that temp is ${FtoC(x)}`);
+
