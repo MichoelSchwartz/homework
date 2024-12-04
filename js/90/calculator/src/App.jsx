9cb2ad6b54530663e0769c085d9af05e5a8a6bde
MichoelSchwartz <michoeldov@gmail.com>
2024-12-03 23:57:10 -0500

    streamlined code with for loops

--- js/90/calculator/src/App.jsx
+++ js/90/calculator/src/App.jsx
@@ -3,23 +3,23 @@ import './App.css'
 
 export default class App extends Component {
   state = {
-    current1: '',
-    current2: '',
+    val1: '',
+    val2: '',
     operator: '',
     isValueOneEntered: false,
     operatorIsEntered: false
   }
 
   handleClick = (e) => {
-    let { current1, current2 } = this.state;
+    let { val1, val2 } = this.state;
     console.log(e);
     if (!this.state.isValueOneEntered) {
       this.setState({
-        current1: current1 += e.target.innerText
+        val1: val1 += e.target.innerText
       });
     } else if (this.state.operatorIsEntered) {
       this.setState({
-        current2: current2 += e.target.innerText
+        val2: val2 += e.target.innerText
       });
     }
   }
@@ -35,8 +35,8 @@ export default class App extends Component {
 
   clearHandleClick = () => {
     this.setState({
-      current1: '',
-      current2: '',
+      val1: '',
+      val2: '',
       operator: '',
       isValueOneEntered: false,
       operatorIsEntered: false
@@ -44,85 +44,58 @@ export default class App extends Component {
   }
 
   equalsHandleClick = (e) => {
-    const { current1, current2, operator } = this.state
+    const { val1, val2, operator } = this.state
     console.log(e);
     if (operator === '+') {
       this.setState({
-        current1: parseInt(current1) + parseInt(current2)
+        val1: parseInt(val1) + parseInt(val2)
       });
     } else if (operator === '-') {
       this.setState({
-        current1: parseInt(current1) - parseInt(current2)
+        val1: parseInt(val1) - parseInt(val2)
       });
     } else if (operator === 'x') {
       this.setState({
-        current1: parseInt(current1) * parseInt(current2)
+        val1: parseInt(val1) * parseInt(val2)
       });
     } else if (operator === '÷') {
       this.setState({
-        current1: parseInt(current1) / parseInt(current2)
+        val1: parseInt(val1) / parseInt(val2)
       });
     }
     this.setState({
-      current2: '',
+      val2: '',
       operator: '',
       operatorIsEntered: false
     })
   }
 
   render() {
-    const { current1, current2, operator } = this.state;
+    const { val1, val2, operator } = this.state;
+    const numButtons = [];
+    for (let i = 0; i < 10; i++) {
+      numButtons.push(<button onClick={
+        this.handleClick
+      }>{i}</button>);
+    }
+    const operatorButtons = [];
+    const operators = ['+', '-', 'x', '÷']
+    for (let i = 0; i < operators.length; i++) {
+      operatorButtons.push(<button onClick={
+        this.operatorHandleClick
+      }>{operators[i]}</button>);
+    }
     return (
       <>
-        <button onClick={
-          this.handleClick
-        }>1</button>
-        <button onClick={
-          this.handleClick
-        }>2</button>
-        <button onClick={
-          this.handleClick
-        }>3</button>
-        <button onClick={
-          this.handleClick
-        }>4</button>
-        <button onClick={
-          this.handleClick
-        }>5</button>
-        <button onClick={
-          this.handleClick
-        }>6</button>
-        <button onClick={
-          this.handleClick
-        }>7</button>
-        <button onClick={
-          this.handleClick
-        }>8</button>
-        <button onClick={
-          this.handleClick
-        }>9</button>
-        <button onClick={
-          this.handleClick
-        }>0</button>
-        <button onClick={
-          this.operatorHandleClick
-        }>+</button>
-        <button onClick={
-          this.operatorHandleClick
-        }>-</button>
-        <button onClick={
-          this.operatorHandleClick
-        }>x</button>
-        <button onClick={
-          this.operatorHandleClick
-        }>÷</button>
+        {numButtons}
+        {operatorButtons}
         <button onClick={
           this.equalsHandleClick
         }>=</button>
         <button onClick={
           this.clearHandleClick
         }>C</button>
-        <div>{current1} {operator} {current2}</div>
+        <div>{val1} {operator} {val2}</div>
       </>
     )
   }
